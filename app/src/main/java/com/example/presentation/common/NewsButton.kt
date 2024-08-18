39f372e19a50428c763180663dfa4807702af791
AlihanCelik <alihan_08032003@hotmail.com>
2024-08-19 02:33:32 +0300

    newsApp

---
+++ app/src/main/java/com/example/presentation/common/NewsButton.kt
@@ -0,0 +1,31 @@
+package com.example.presentation.common
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NewButton(
+    text:String,
+    onClick:()->Unit
+){
+    Button(onClick = onClick, colors = ButtonDefaults.buttonColors(
+        contentColor = Color.White,
+        containerColor = MaterialTheme.colorScheme.primary
+        ),
+        shape = RoundedCornerShape(size = 6.dp )
+        ) {
+            Text(text = text,
+                style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold))
+
+    }
+
+}
