5b46fb7d44d147c274e2fe62f58e5d254eb1b8d3
AlihanCelik <alihan_08032003@hotmail.com>
2024-08-19 03:08:43 +0300

    newsApp

--- app/src/main/java/com/example/presentation/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/example/presentation/onboarding/OnBoardingScreen.kt
@@ -3,7 +3,6 @@ package com.example.presentation.onboarding
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
