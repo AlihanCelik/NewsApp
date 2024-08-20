7448253062ae0e5d74fbd46a0bc8d75e3b284937
AlihanCelik <alihan_08032003@hotmail.com>
2024-08-20 04:34:47 +0300

    newsApp

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -10,9 +10,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.lifecycleScope
 import com.example.newsapp.domain.usercases.AppEntryUsesCases
 import com.example.newsapp.presentation.onboarding.OnBoardingScreen
+import com.example.newsapp.presentation.onboarding.OnBoardingViewModel
 import com.example.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -22,21 +24,26 @@ import javax.inject.Inject
 class MainActivity : ComponentActivity() {
 
     @Inject
-    lateinit var appEntryUsesCases: AppEntryUsesCases
+    lateinit var usesCases: AppEntryUsesCases
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window,false)
         installSplashScreen()
         lifecycleScope.launch {
-            appEntryUsesCases.readAppEntry().collect{
-                Log.d("Test",it.toString())
+            usesCases.readAppEntry().collect{
+                Log.d("test",it.toString())
             }
         }
         setContent {
             NewsAppTheme {
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)){
-                    OnBoardingScreen()
+                    val viewModel:OnBoardingViewModel= hiltViewModel()
+                    OnBoardingScreen(
+                        event = {
+                            viewModel::onEvent
+                        }
+                    )
                 }
 
             }
--- app/src/main/java/com/example/newsapp/presentation/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/example/newsapp/presentation/onboarding/OnBoardingScreen.kt
@@ -28,7 +28,9 @@ import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun OnBoardingScreen(){
+fun OnBoardingScreen(
+    event: (OnBoardingEvent)->Unit
+){
     Column(modifier = Modifier.fillMaxSize()) {
         val pagerState= rememberPagerState(initialPage = 0) {
             pages.size
@@ -77,8 +79,8 @@ fun OnBoardingScreen(){
                 NewButton(text = buttonState.value[1],
                     onClick = {
                         scope.launch {
-                            if (pagerState.currentPage == 3) {
-
+                            if (pagerState.currentPage == 2) {
+                                event(OnBoardingEvent.SaveAppEntry)
                             } else  {
                                 pagerState.animateScrollToPage(page = pagerState.currentPage + 1)
                             }
