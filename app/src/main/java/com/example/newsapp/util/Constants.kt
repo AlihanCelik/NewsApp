eed0479aa9cbb15e4ba6817e1bf5ef0711248a74
AlihanCelik <alihan_08032003@hotmail.com>
2024-08-23 11:42:52 +0300

    newsApp

--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -5,4 +5,5 @@ object Constants {
     const val APP_ENTRY="appEntry"
     const val API_KEY="0424a8503e4a44cb850141c4595cae48"
     const val BASE_URL="https://newsapi.org/v2/"
+    const val NEWS_DATABASE_NAME="news_db"
 }
